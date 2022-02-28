bdd1fb2dd6f8fc1fa10387bb6582baa8532278d2
paffen <pavel.stoyanov0@hm.edu>
2022-02-28 18:32:51 +0100

    add a onClick func. with onVideoSelect option

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { videos: [] , selectedVideo: null };
 
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
@@ -12,14 +12,20 @@ class App extends React.Component {
                 q: term
             }
         })
-        console.log(response);
-        //this.setState({ videos: response.data.items })
+        //console.log(response);
+        this.setState({ videos: response.data.items })
     };
+    onVideoSelect = (video) => {
+        console.log('From the App!',video);
+    };
+
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
+                <VideoList 
+                onVideoSelect={this.onVideoSelect} 
+                videos={this.state.videos} />
             </div>
         );
     }
