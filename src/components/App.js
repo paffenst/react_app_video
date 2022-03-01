328619f3db90cf94561ae3d21b921b783da2e7d6
paffen <pavel.stoyanov0@hm.edu>
2022-03-01 11:37:39 +0100

    finish the search video app v.1.0

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,10 @@ import VideoDetail from './VideoDetail';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('buildings');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: {
@@ -14,9 +18,12 @@ class App extends React.Component {
             }
         })
         //console.log(response);
-        this.setState({ videos: response.data.items })
+        this.setState({ 
+            videos: response.data.items ,
+            selectedVideo : response.data.items[0]
+        });
     };
-    onVideoSelect = (video) => {
+    onVideoSelect = video => {
         this.setState({ selectedVideo: video });
     };
 
